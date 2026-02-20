24768bedaefaafb2ddf44a2d245aefcaa5a94880
Sárosi Márton <sarosi.marton20k@egressy.info>
2026-02-20 13:04:00 +0100

    WorkerModel, AdminModel created AdminRegister,WorkerRegister AdminValidate, WorkerValidate

--- WebShop/WebShop/Model/AdminModel.cs
+++ WebShop/WebShop/Model/AdminModel.cs
@@ -1,6 +1,44 @@
-﻿namespace WebShop.Model
+﻿using System.Security.Cryptography;
+using System.Text;
+using WebShop.Persistence;
+
+namespace WebShop.Model
 {
     public class AdminModel
     {
+        private readonly DataDbContext _context;
+        public AdminModel(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AdminRegistration(string username, string password, string role = "Admin")
+        {
+            if (_context.Admins.Any(x => x.AdminName == username))
+            {
+                throw new InvalidOperationException("Admin already exists");
+            }
+            using var trx = _context.Database.BeginTransaction();
+            {
+                _context.Admins.Add(new Admin { AdminName = username, Password = HashPassword(password), Role = role });
+                _context.SaveChanges();
+                trx.Commit();
+            }
+        }
+
+        public Admin? ValidateAdmin(string username, string password)
+        {
+            var hash = HashPassword(password);
+            var admin = _context.Admins.Where(x => x.AdminName == username);
+            return admin.Where(x => x.Password == hash).FirstOrDefault();
+        }
+
+        private string HashPassword(string password)
+        {
+            using var sha = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hash = sha.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
     }
 }
