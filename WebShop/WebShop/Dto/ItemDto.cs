a4245b5a715017a00e9d8c2aa901a9ac424daa75
Sárosi Márton <sarosi.marton20k@egressy.info>
2026-02-20 13:11:11 +0100

    ItemDto created

--- WebShop/WebShop/Dto/ItemDto.cs
+++ WebShop/WebShop/Dto/ItemDto.cs
@@ -1,6 +1,15 @@
-﻿namespace WebShop.Dto
+﻿using WebShop.Persistence;
+
+namespace WebShop.Dto
 {
     public class ItemDto
     {
+        public int itemId {  get; set; }
+        public int categoryId { get; set; }
+        public string itemName { get; set; }
+        public int quantity { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+        public List<Specs> specs { get; set; }
     }
 }
