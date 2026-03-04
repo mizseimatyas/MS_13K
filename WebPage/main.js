17f5489f435bbb0cd2f2f4718f70826149a3eb24
Sárosi Márton <sarosi.marton20k@egressy.info>
2026-03-04 12:35:49 +0100

    WebPaged start

---
+++ WebPage/main.js
@@ -0,0 +1,12 @@
+let port = 7171
+
+const rmail = document.getElementById("rmail")
+const rpass = document.getElementById("rpass")
+const raddress = document.getElementById("raddress")
+const rphone = document.getElementById("rphone")
+const registerbtn = document.getElementById("register_btn")
+
+
+
+
+const loginbtn = document.getElementById("login_btn")
