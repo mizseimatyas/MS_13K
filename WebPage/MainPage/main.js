9d17b3ae37483986e6318814b298002cf440b825
mizsei.matyas <mizsei.matyas@C113-05>
2026-03-05 09:54:12 +0100

    Register and LoginPage Done.

--- WebPage/MainPage/main.js
+++ WebPage/MainPage/main.js
@@ -1,1 +1,16 @@
 let port = 7171
+
+//Dark/Light Button
+const btn = document.getElementById("themeBtn");
+
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    if (btn.classList.contains("btn-light")) {
+        btn.classList.remove("btn-light");
+        btn.classList.add("btn-dark")
+    } 
+    else {
+        btn.classList.remove("btn-dark");
+        btn.classList.add("btn-light");
+    }
+});
