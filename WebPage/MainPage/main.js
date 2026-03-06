c551e7452f753dcbb5a3395a6df8891a0bcca8dc
mizsei.matyas <mizsei.matyas@C113-05>
2026-03-06 12:35:58 +0100

    Worked on MainPage.

--- WebPage/MainPage/main.js
+++ WebPage/MainPage/main.js
@@ -1,1 +1,21 @@
 let port = 7171
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+window.addEventListener("resize", () => {
+    const menu = document.getElementById("mobileMenu");
+    const desktopMenu = document.querySelector(".desktop-menu");
+    const hamburger = document.querySelector(".hamburger");
+    if (window.innerWidth > 768) {
+        desktopMenu.style.display = "flex";
+        hamburger.style.display = "none";
+        menu.style.display = "none";
+    } 
+    else {
+        desktopMenu.style.display = "none";
+        hamburger.style.display = "block";
+    }
+});
